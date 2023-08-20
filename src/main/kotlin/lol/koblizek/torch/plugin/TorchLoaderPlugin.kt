fb93101c77f27cd50777244d4f9b4e8e54c498ba
KoblizekXD <janprokupek@centrum.cz>
2023-08-20 19:51:26 +0200

    feat: plugin logging

--- src/main/kotlin/lol/koblizek/torch/plugin/TorchLoaderPlugin.kt
+++ src/main/kotlin/lol/koblizek/torch/plugin/TorchLoaderPlugin.kt
@@ -2,13 +2,18 @@ package lol.koblizek.torch.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.logging.Logger
 
 /**
  * Main plugin entrypoint
  */
 class TorchLoaderPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
+    override fun apply(project: Project) {
+        logger = project.logger
+    }
 
+    companion object {
+        internal lateinit var logger: Logger
     }
 }
 
