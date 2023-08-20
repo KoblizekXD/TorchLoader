aa1e0979bba6639d2007e3c06c044bdff0a2b4e7
KoblizekXD <janprokupek@centrum.cz>
2023-08-20 19:41:05 +0200

    fix: added initialization checking functions to prevent null errors

--- src/main/kotlin/lol/koblizek/torch/plugin/ModProject.kt
+++ src/main/kotlin/lol/koblizek/torch/plugin/ModProject.kt
@@ -3,8 +3,13 @@ package lol.koblizek.torch.plugin
 class ModProject {
     companion object {
         lateinit var modProjectInstance: ModProject
+
+        fun isModProjectInitialized(): Boolean = ::modProjectInstance.isInitialized
     }
 
     lateinit var minecraft: String
     lateinit var mappings: String
+
+    fun isMinecraftInitialized(): Boolean = ::minecraft.isInitialized
+    fun areMappingsInitialized(): Boolean = ::minecraft.isInitialized
 }
