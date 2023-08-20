72afd16880d4b762b2ebc2562fee6888a00d46c0
KoblizekXD <janprokupek@centrum.cz>
2023-08-20 20:16:02 +0200

    feat: added execution block into EvaluatedTask

--- src/main/kotlin/lol/koblizek/torch/plugin/tasks/EvaluatedTask.kt
+++ src/main/kotlin/lol/koblizek/torch/plugin/tasks/EvaluatedTask.kt
@@ -3,5 +3,13 @@ package lol.koblizek.torch.plugin.tasks
 import org.gradle.api.Project
 
 abstract class EvaluatedTask {
+    abstract val name: String
+
+
+    fun execute(project: Project) {
+        println("task :$name")
+        onEvaluation(project)
+    }
+
     abstract fun onEvaluation(project: Project)
 }
